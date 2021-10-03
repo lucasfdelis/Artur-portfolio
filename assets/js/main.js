c81b0ed504fafce776cd82df0f9d5df1d843c3ec
Lucas <lucasbivarfonseca@gmail.com>
2021-10-03 15:54:51 -0300

    adding dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,52 @@
+const setLight = () => {
+    const cssVariables = document.documentElement.style;
+    cssVariables.setProperty('--first-color', '#4070F4');
+    cssVariables.setProperty('--second-color', '#0E2431');
+    cssVariables.setProperty('--btn-text', '#fff');
+    cssVariables.setProperty('--skills-bg', 'rgb(250, 250, 250)');
+    cssVariables.setProperty('--background-color', '#fff');
+    cssVariables.setProperty('--footer-color', '#0c1c29'); 
+
+    document.getElementById('btn-image').src='assets/img/moon.svg';
+}
+
+const setDark = () => {
+    const cssVariables = document.documentElement.style;
+    cssVariables.setProperty('--first-color', '#91FF16');
+    cssVariables.setProperty('--second-color', '#fff');
+    cssVariables.setProperty('--btn-text', '#0E2431');
+    cssVariables.setProperty('--skills-bg', '#0c1c29');
+    cssVariables.setProperty('--background-color', '#0E2431');
+    cssVariables.setProperty('--footer-color', '#0c1c29');
+
+    document.getElementById('btn-image').src='assets/img/sun.svg';
+}
+
+const getThemeOnLoad = () => {
+    const persistedValue = localStorage.getItem('theme');
+    if(persistedValue === null){
+        persistedValue = 'light'
+        localStorage.setItem('theme', persistedValue);
+    }else if(persistedValue === 'dark'){
+        setDark();
+    }
+}
+
+window.onload = () => {
+    getThemeOnLoad();
+}
+
+document.getElementById('btn-theme').addEventListener('click', () => {
+        const currentTheme = localStorage.getItem('theme');
+        if(currentTheme === 'dark'){
+            setLight();
+            localStorage.setItem('theme', 'light');
+        }else{
+            setDark();
+            localStorage.setItem('theme', 'dark');
+        }
+    });
+
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
